990e06bf35014b35c9e38d0a9f05c7a168bef9ef
Andrey Oveshkov <132068119+Drytimor@users.noreply.github.com>
2024-03-28 18:40:39 +0300

    Заменить txt на csv

--- app/main.py
+++ app/main.py
@@ -8,10 +8,20 @@ from collections import Counter
 import db
 from datetime import datetime
 from math import log10
+import csv
+import os
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(level=logging.INFO)
+file_handler = logging.FileHandler(filename='log.log', encoding='utf-8', mode='w')
+formatter = logging.Formatter('%(name)s-%(message)s')
+file_handler.setFormatter(formatter)
+log.addHandler(file_handler)
 
 
 app = FastAPI()
-FILE_PATH = './text/{filename}'
+DIR_PATH_DOWNLOAD = './text'
 
 
 
@@ -30,11 +40,10 @@ async def upload_file(
         request: Request, file: UploadFile, session: db.SessionDep,
 ):
     _bytes = await file.read()
-    filepath, filename = datetime.now().strftime('%Y%m%d%H%S') + file.filename, file.filename
     total_number_words, word_list, prepare_string_words = await prepare_data(_bytes)
     file_data_from_request = dict(Counter(word_list))
     result_TF_IDF_file, all_files_from_db = await TF_IDF_func(
-        total_number_words, file_data_from_request, prepare_string_words, filepath, filename, session
+        total_number_words, file_data_from_request, prepare_string_words, file.filename, session
     )
     return templates.TemplateResponse(
             request=request, name='table.html',
@@ -65,14 +74,20 @@ async def prepare_data(_bytes: bytes):
 
 async def TF_IDF_func(
         total_number_words: int, file_data_from_request: dict | None,
-        prepare_string_words: str, filepath: str, filename:str,
-        session: AsyncSession
+        filename:str, session: AsyncSession
 ):
     if file_data_from_request:
-        with open(FILE_PATH.format(filename=filepath), 'w') as f:
-            f.write(prepare_string_words)
+        file_csv = re.sub(r'\.txt', '.csv', filename)
+        filepath = os.path.join(DIR_PATH_DOWNLOAD, datetime.now().strftime('%Y%m%d%H%S') + file_csv)
+        with open(filepath, 'w', newline='') as csvfile:
+            writer = csv.DictWriter(
+                csvfile, fieldnames=file_data_from_request.keys(), quoting=csv.QUOTE_NONNUMERIC
+            )
+            writer.writeheader()
+            writer.writerow(file_data_from_request)
+
             files_data = db.FilesBase(
-                file_path=f.name, file_name=filename, number_word=total_number_words
+                file_path=filepath, file_name=filename, number_word=total_number_words
             )
             await db.add_file_to_db(session=session, files_data=files_data)
 
@@ -93,12 +108,17 @@ async def TF_IDF_func(
 
     for file in all_files_from_db:
         total_number_documents += 1
-        with open(file.file_path, 'r') as f:
-            text = f.read()
+        with open(file.file_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+            for row in reader:
+                row = row
+                break
+
             for word in file_data_from_request:
                 response_result_dict[word].setdefault('num', 0)
-                if re.search(word, text):
+                if row.get(word):
                     response_result_dict[word]['num'] += 1
+                    continue
 
     for word, data in response_result_dict.items():
         number_documents_with_word = data.pop('num')
@@ -113,21 +133,3 @@ def TF_Function(word_frequency: int, total_number_words: int) -> float:
 
 async def IDF_Function(total_number_documents, number_documents_with_word) -> float:
     return round(log10(total_number_documents / number_documents_with_word), 4)
-
-"""
-async def count_number_words_in_text(file_from_db) -> dict:
-    with open(file_from_db, 'r') as f:
-        text = f.read()
-        list_words = [word for word in text.split()]
-        count_words_with_file = dict(Counter(list_words))
-        return {
-            word: {'TF': TF_Function(count, total_number_words)} for word, count in count_words_with_file.items()
-        }
-"""
-
-
-
-
-
-
-
